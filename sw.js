c6468a1375217581476069b43fc79739316259b2
SoCooltw <qwdfzxc123@gmail.com>
2026-05-12 16:07:27 +0800

    Move mobile controls to landscape rail

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'snake-game-tw-v4';
+const CACHE_NAME = 'snake-game-tw-v5';
 const APP_SHELL = [
     '/',
     '/index.html',
-    '/style.css?v=5',
-    '/script.js?v=5',
+    '/style.css?v=6',
+    '/script.js?v=6',
     '/manifest.webmanifest',
     '/icons/icon-192.svg',
     '/icons/icon-512.svg'
